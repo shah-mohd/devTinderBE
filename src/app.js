3e8cbc267aab2d560afa4945ed3419a3dc6a2619
Shah Mohammad Ansari <shahmohammadt01@gmail.com>
2025-07-22 15:46:06 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -3,28 +3,39 @@ const {adminAuth, userAuth} = require("./middlewares/auth");
 
 const app = express();
 
-app.use("/admin", adminAuth);
+// app.use("/admin", adminAuth);
 
-app.post("/user/login", (req, res) => {
-    res.send("login user");
+// app.post("/user/login", (req, res) => {
+//     res.send("login user");
+// });
+
+// app.use("/user", userAuth);
+
+// app.get("/user/data", (req, res) => {
+//     res.send("user data");
+// });
+
+// app.get("/user/profile", (req, res) => {
+//     res.send("profile data");
+// });
+
+// app.get("/admin/getAllUsers", (req, res) => {
+//     res.send("All data sent!");
+// });
+
+// app.get("/admin/deleteUser", (req, res) => {
+//     res.send("Deleted a user!");
+// });
+
+
+app.use("/", (req, res) => {
+    throw new Error("there are error!");
 });
 
-app.use("/user", userAuth);
-
-app.get("/user/data", (req, res) => {
-    res.send("user data");
-});
-
-app.get("/user/profile", (req, res) => {
-    res.send("profile data");
-});
-
-app.get("/admin/getAllUsers", (req, res) => {
-    res.send("All data sent!");
-});
-
-app.get("/admin/deleteUser", (req, res) => {
-    res.send("Deleted a user!");
+app.use("/", (err, req, res, next) => {
+    if(err){
+        res.status(500).send("error from err");
+    }
 });
 
 
