fbd9c3b7d6455f1809b409c821dc90c3dd5deee8
Shah Mohammad Ansari <shahmohammadt01@gmail.com>
2025-07-22 10:13:06 +0530

    explore GET POST DELETE calls

--- src/app.js
+++ src/app.js
@@ -2,17 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-    res.send("Hello, Shah!");
-})
+// this will match hndle GET call to /user
+app.get("/user", (req, res) => {
+    res.send({firstName: "Shah", lastName: "Mohd"});
+});
 
-app.use("/hello", (req, res) => {
-    res.send("Hello from the server");
-})
+app.post("/user", (req, res) => {
+    // saving data to DB
+    res.send("data successfully saved to the database");
+});
 
+app.delete("/user", (req, res) => {
+    res.send("deleted successfully");
+});
+
+// this will match all the http API calls to /test
 app.use("/test", (req, res) => {
     res.send("test from the server");
-})
+});
 
 
 app.listen("7777", () => {
