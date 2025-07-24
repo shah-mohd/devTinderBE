8af432984826dbd57eb30ebf5b87550fc29dccf0
Shah Mohammad Ansari <shahmohammadt01@gmail.com>
2025-07-24 22:08:05 +0530

    explore validator.js library

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -16,10 +17,20 @@ const userSchema = new mongoose.Schema({
         lowercase: true,
         unique: true,
         trim: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid email address " + value);
+            }
+        }
     },
     password: {
         type: String,
         required: true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Enter a strong password " + value);
+            }
+        }
     },
     age: {
         type: Number,
@@ -37,6 +48,11 @@ const userSchema = new mongoose.Schema({
     photoUrl: {
         type: String,
         default: "https://geographyandyou.com/images/user-profile.png",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("Invalid photo URL " + value);
+            }
+        }
     },
     about: {
         type: String,
