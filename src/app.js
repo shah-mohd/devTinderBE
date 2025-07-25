a55576044af8f9746240d324a4b77e8061421f31
Shah Mohammad Ansari <shahmohammadt01@gmail.com>
2025-07-25 16:58:57 +0530

    userscheme mthods

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const User = require("./models/user");
 const {validateSignUpData} = require("./utils/validation");
 const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
-const jwt = require("jsonwebtoken");
 const {userAuth} = require("./middlewares/auth");
 
 const app = express();
@@ -45,10 +44,10 @@ app.post("/login", async (req, res) => {
             throw new Error("Invalid credential!");
         }
 
-        const isPasswordValid = await bcrypt.compare(password, user.password);
+        const isPasswordValid = await user.validatePassword(password);
         if(isPasswordValid){
             // create JWT token
-            const token = await jwt.sign({_id: user._id}, "DEV@Tender$790", {expiresIn: "7d"});
+            const token = await user.getJWT();
 
             // add the token to cookie and send the response back to the user
             res.cookie("token", token, {expires: new Date(Date.now() + 178 * 3600000)});
