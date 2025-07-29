666db2e44a670a52582329821175aebb0e7a0d41
Shah Mohammad Ansari <shahmohammadt01@gmail.com>
2025-07-29 10:08:33 +0530

    connection APIs

---
+++ src/routes/user.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const userRouter = express.Router();
+
+
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
+
+// Get all the pending connection request for the loggedIn user
+userRouter.get("/user/requests/received", userAuth, async (req, res) => {
+    try{
+        const loggedInUser = req.user;
+        const connectionRequest = await ConnectionRequest.find({
+            toUserId: loggedInUser._id,
+            status: "interested"
+        }).populate("fromUserId", USER_SAFE_DATA);
+        // }).populate("fromUserId", ["firstName", "lastName"]);
+
+        res.json({
+            message: "Data fetch successfully",
+            data: connectionRequest
+        });
+    } catch(err) {
+        res.status(400).send("ERROR : " + err.message);
+    }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+    try{
+        const loggedInUser = req.user;
+        const connectionRequest = await ConnectionRequest.find({
+            $or: [
+                {toUserId: loggedInUser._id, status: "accepted"},
+                {fromUserId: loggedInUser._id, status: "accepted"}
+            ]
+        }).populate("fromUserId", USER_SAFE_DATA).populate("toUserId", USER_SAFE_DATA);
+
+        const data = connectionRequest.map((row) => {
+            if(row.fromUserId._id.toString() === loggedInUser._id.toString()){
+                return row.toUserId;
+            }
+            return row.fromUserId; 
+        });
+
+        res.json({data});
+    } catch(err) {
+        res.status(400).send("ERROR " + err.message);
+    }
+});
+
+module.exports = userRouter;
