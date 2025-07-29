75493e11945a80959b404dcdbf551f234a4eff65
Shah Mohammad Ansari <shahmohammadt01@gmail.com>
2025-07-29 20:34:36 +0530

    issue in userAuth

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,7 +5,8 @@ const userAuth = async (req, res, next) => {
     try{
         const {token} = req.cookies;
         if(!token){
-            res.status(401).send("Please Login!");
+            throw new Error("Token is not valid!");
+            // res.status(401).send("Please Login!");
         }
         const decodeObj = await jwt.verify(token, "DEV@Tender$790");
         const {_id} = decodeObj;
