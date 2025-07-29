43a35775b45a408ae6b0c1e3a7df6bf337a03603
Shah Mohammad Ansari <shahmohammadt01@gmail.com>
2025-07-29 20:14:01 +0530

    userAuth middleware error handle good way

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,7 +5,7 @@ const userAuth = async (req, res, next) => {
     try{
         const {token} = req.cookies;
         if(!token){
-            throw new Error("Token is not valid!");
+            res.status(401).send("Please Login!");
         }
         const decodeObj = await jwt.verify(token, "DEV@Tender$790");
         const {_id} = decodeObj;
